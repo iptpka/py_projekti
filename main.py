6e63d214060e5e2748ed00182c091c2f916c3f33
iptpka <tuomas.jarvenkyla@edu.turkuamk.fi>
2023-04-16 01:07:31 +0300

    crude viewport and curve

--- main.py
+++ main.py
@@ -1,43 +1,78 @@
 # Example file showing a circle moving on screen
-import pygame
+import pygame as pg
+import pygame.gfxdraw as gfx
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
+class curve:
+    def __init__(self, points, steps = 100, color = (0, 0, 0)):
+        self.points = points
+        self.steps = steps
+        self.color = color
+
+    def draw(self, surface):
+        gfx.bezier(surface, self.points, self.steps, self.color)
+        
+
+
+    
+
+
+# pyg setup
+pg.init()
+screen = pg.display.set_mode((1280, 720))
+clock = pg.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+viewport_width_percent = 0.8
+viewport_height_percent = 0.9
+editor_viewport = pg.Surface((screen.get_width()*viewport_width_percent, screen.get_height()*viewport_height_percent))
+editor_viewport.set_colorkey((0, 255, 255))
+viewport_x_offset = screen.get_width() * (1 - viewport_width_percent) / 4
+viewport_y_offset = screen.get_height() * (1 - viewport_height_percent) / 2
+
+
+vector_surface = pg.Surface((1280, 720))
+vector_surface.set_colorkey((255, 255, 255))
+test_curve = curve([(0, 0), (200, 200), (300, 100), (400, 200)], 100, (0, 0, 0))
+
+player_pos = pg.Vector2(editor_viewport.get_width() / 2, editor_viewport.get_height() / 2)
 
 while running:
     # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    # pg.QUIT event means the user clicked X to close your window
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("grey")
+    vector_surface.fill((255, 255, 255))
+    editor_viewport.fill((255, 255, 255))
+    test_curve.draw(vector_surface)
+    editor_viewport.blit(vector_surface, player_pos)
+    screen.blit(editor_viewport, (viewport_x_offset, viewport_y_offset))
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
+    keys = pg.key.get_pressed()
+    if keys[pg.K_w]:
         player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
+    if keys[pg.K_s]:
         player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
+    if keys[pg.K_a]:
         player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
+    if keys[pg.K_d]:
         player_pos.x += 300 * dt
-
+    
     # flip() the display to put your work on screen
-    pygame.display.flip()
+    pg.display.flip()
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
     dt = clock.tick(60) / 1000
 
-pygame.quit()
+pg.quit()
+
+
+
+
